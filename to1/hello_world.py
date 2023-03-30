79b2e91d9dca7dd05e965e774e03412b1fbe0d8b
Anton-Ilyk <129430677+Anton-Ilyk@users.noreply.github.com>
2023-03-30 20:41:30 +0100

    Delete to1 directory

--- to1/hello_world.py
+++
@@ -1,8 +0,0 @@
-#Getting and displaying user`s name and age
-
-name = input("What is your name?\r\n")
-age = int(input("What is your age?\r\n"))
-
-print("{} your age is {} years!\r\n".format(name, age))
-
-print("Hello, World!")
--- to1/readme.txt
+++
@@ -1,1 +0,0 @@
-My first programms made on Python
